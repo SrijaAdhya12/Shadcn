aa3b1b2038303ca15ec14b3e831d2d1f52f8a02d
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-09-07 02:02:19 +0530

    feat: fix navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,6 +11,9 @@ import {
 import { Input } from '@/components/ui/input'
 import { Logo } from '@/assets'
 
+
+
+
 const Navbar = () => {
 	return (
 		<nav className="border flex items-center h-20 px-8 sm:px-20 font-roboto sticky top-0 z-50 py-3 backdrop-blur-md border-neutral-700/80">
@@ -18,9 +21,16 @@ const Navbar = () => {
 				<div>
 					<NavigationMenu>
 						<NavigationMenuList>
-							<NavigationMenuItem>
-								<img src={Logo} alt="Logo" className="h-8 w-8" />
-							</NavigationMenuItem>
+							<div className='flex items-center'>
+								<div>
+									<NavigationMenuItem>
+										<img src={Logo} alt="Logo" className="h-8 w-8" />
+									</NavigationMenuItem>
+								</div>
+								<div className='flex gap-4'>
+									<NavigationMenuItem>Home</NavigationMenuItem>
+								</div>
+							</div>
 						</NavigationMenuList>
 					</NavigationMenu>
 				</div>
