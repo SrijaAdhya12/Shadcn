a5cac01dfb08c1f68de49a792e5ab64990b81663
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-09-07 12:38:47 +0530

    feat: fixed navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
 			<div className="flex items-center justify-between w-full">
 				<div className="flex items-center gap-4">
 					<NavigationMenu>
-						<NavigationMenuList className="flex items-center space-x-24 sm:space-x-[580px]">
+						<NavigationMenuList className="flex items-center space-x-8 sm:space-x-[580px]">
 							<div>
 								<NavigationMenuItem>
 									<img src={Logo} alt="Logo" className="h-8 w-8" />
